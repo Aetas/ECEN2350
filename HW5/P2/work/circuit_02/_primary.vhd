218dca171840b95f9822a60fc87db086402bccf0
Derek Prince <ace.prince08@gmail.com>
2016-09-25 20:39:02 -0600

    p2 circuits

---
+++ HW5/P2/work/circuit_02/_primary.vhd
@@ -0,0 +1,11 @@
+library verilog;
+use verilog.vl_types.all;
+entity circuit_02 is
+    port(
+        y2              : out    vl_logic;
+        x1              : in     vl_logic;
+        x2              : in     vl_logic;
+        x3              : in     vl_logic;
+        x4              : in     vl_logic
+    );
+end circuit_02;
