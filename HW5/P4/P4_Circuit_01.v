1adc15fc4fb189e255eddca5a11ad5ea43620ef1
Derek Prince <ace.prince08@gmail.com>
2016-09-25 21:41:51 -0600

    Added circuit 1

---
+++ HW5/P4/P4_Circuit_01.v
@@ -0,0 +1,20 @@
+/*************************
+ * Derek Prince          *
+ * Problem Set #5        *
+ * Problem 4             *
+ * Circuit 1 description *
+*************************/
+
+module circuit_01(y, x1, x2, x3);
+// outputs
+output y;
+input x1, x2, x3;
+
+// circuit behaviour
+assign a = (x1&x2);
+assign b = (x1&~x2&~x3);
+assign c = (~x1&x2&x3);
+// output
+assign y = (a|b|c);
+
+endmodule
